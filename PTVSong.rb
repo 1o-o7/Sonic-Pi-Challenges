0c6fb6494f532a61a198343573ce1bc1fc40e8c6
1o-o7 <jocelyn_vazquez@student.davincischools.org>
2025-03-13 11:33:06 -0700

    PTVSong.rb

--- PTVSong.rb
+++ PTVSong.rb
@@ -24,6 +24,23 @@ define :veil do
   sleep 3
 end
 
+define :para do |sound1, sound2, sound3, sound4, sound5, sound6, sound7|
+  play sound1
+  sleep 0.5
+  play sound2
+  sleep 0.5
+  play sound3
+  sleep 0.5
+  play sound4
+  sleep 1
+  play sound5
+  sleep 0.5
+  play sound6
+  sleep 0.5
+  play sound7
+  sleep 0.5
+end
+
 define :repeat do
   play :Bb3, amp: 1.5
   sleep 0.5
@@ -37,9 +54,11 @@ end
 
 drums_sample = "C:/Users/jocelyn_vazquez/Downloads/HOTM- Drums.mp3"
 bass_sample = "C:/Users/jocelyn_vazquez/Downloads/bass.mp3"
-extra_sample ="C:/Users/jocelyn_vazquez/Downloads/extra-sound.mp3"
-drums_2_sample ="C:/Users/jocelyn_vazquez/Downloads/drums2.mp3"
+extra_sample = "C:/Users/jocelyn_vazquez/Downloads/extra-sound.mp3"
+drums_2_sample = "C:/Users/jocelyn_vazquez/Downloads/drums2.mp3"
 
+notesList = [:G2, :Ab2, :Eb3, :Bb2, :Ab2, :G2, :Ab2,:Eb3, ]
+sleepList = [0.25, 0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.25,]
 
 live_loop :hold do
   use_synth :dpulse
@@ -65,25 +84,20 @@ live_loop :hold do
   
   use_synth :subpulse
   8.times do
-    play :G2, amp: 0.8
-    sleep 0.25
-    play :Ab2, amp: 0.8
-    sleep 0.25
-    play :Eb3, amp: 0.8
-    sleep 0.25
-    play :Bb2, amp: 0.8
-    sleep 0.5
-    play :Ab2, amp: 0.8
-    sleep 0.5
-    play :G2, amp: 0.8
-    sleep 0.25
-    play :Ab2, amp: 0.8
-    sleep 0.25
-    play :Eb3, amp: 0.8
-    sleep 0.25
-    play :Bb2, sustain: 1.5, amp: 0.8
+    n = 0
+    s = 0
+    a = 0.4
+    8.times do
+      play notesList[n], amp: a
+      sleep sleepList[s]
+      n = n + 1
+      s = s + 1
+      a = a + 0.1
+    end
+    
+    play :Bb2, sustain: 1.5, amp: 1
     sleep 1.5
-    play :Ab2, amp: 0.8
+    play :Ab2, amp: 1
     sleep 0.5
   end
   stop
@@ -118,15 +132,15 @@ live_loop :on do
       sleep 2
       
       sleep 1.5
-      play :Bb3, amp: 1.5
+      play :Bb3
       sleep 0.5
-      play :Eb4, amp: 1.5
+      play :Eb4
       sleep 0.5
-      play :F4, amp: 1.5
+      play :F4
       sleep 0.5
-      play :Eb4, amp: 1.5
+      play :Eb4
       sleep 0.5
-      play :F4, amp: 1.5
+      play :F4
       sleep 1
       
       sleep 0.5
@@ -204,9 +218,15 @@ end
 sleep 12
 
 live_loop :bass do
-  1.times do
-    sample bass_sample, amp: 2.5
-    sleep 32
+  x = 0
+  y = 0.25
+  z = 1.5
+  4.times do
+    sample bass_sample, start: x, finish: y, amp: z
+    sleep 20.738/4
+    x = x + 0.25
+    y = y + 0.25
+    z = z + 0.5
   end
   stop
 end
@@ -220,6 +240,7 @@ live_loop :extra do
   end
   stop
 end
+
 
 use_synth :dpulse
 live_loop :main do
@@ -238,50 +259,11 @@ live_loop :main do
       play :Eb4
       sleep 0.5
       
-      play :F4
-      sleep 0.5
-      play :G4
-      sleep 0.5
-      play :Ab4
-      sleep 0.5
-      play :G4
-      sleep 1
-      play :Bb3
-      sleep 0.5
-      play :Eb4
-      sleep 0.5
-      play :F4
-      sleep 0.5
+      para :F4, :G4, :Ab4, :G4, :Bb3, :Eb4, :F4
       
-      play :F4
-      sleep 0.5
-      play :G4
-      sleep 0.5
-      play :F4
-      sleep 0.5
-      play :F4
-      sleep 1
-      play :Eb4
-      sleep 0.5
-      play :Eb4
-      sleep 0.5
-      play :Bb4
-      sleep 0.5
+      para :F4, :G4, :F4, :F4, :Eb4, :Eb4, :Bb4
       
-      play :Bb4
-      sleep 0.5
-      play :Ab4
-      sleep 0.5
-      play :G4
-      sleep 0.5
-      play :F4
-      sleep 1
-      play :Eb4
-      sleep 0.5
-      play :Eb4
-      sleep 0.5
-      play :F4
-      sleep 0.5
+      para :Bb4, :Ab4, :G4, :F4, :Eb4, :Eb4, :F4
       
       play :G4
       sleep 0.5
@@ -317,20 +299,7 @@ live_loop :main do
       play :Bb4
       sleep 0.5
       
-      play :Bb4
-      sleep 0.5
-      play :Ab4
-      sleep 0.5
-      play :G4
-      sleep 0.5
-      play :F4
-      sleep 1
-      play :G4
-      sleep 0.5
-      play :F4
-      sleep 0.5
-      play :Eb4
-      sleep 0.5
+      para :Bb4, :Ab4, :G4, :F4, :G4, :F4, :Eb4
       
       play :F4
       sleep 0.5
@@ -345,17 +314,17 @@ live_loop :main do
       play :Bb4
       sleep 1
       
-      play :Bb4, sustain: 1.5
-      play :Eb4, sustain: 1.5
+      play :Bb4, sustain: 1.5, amp: 0.5
+      play :Eb4, sustain: 1.5, amp: 0.5
+      play :F4, sustain: 1.5, amp: 0.5
+      
       sleep 2
     end
     stop
   end
 end
 
-
 sleep 16
-
 
 live_loop :drums_2 do
   1.times do
